c1bfa8de4cd0c7a649b7589d6b113e414765bcde
Omar Gabr <ogabr@westmonroe.com>
2023-07-12 18:55:39 -0500

    please god work

--- create-ecr-stack.sh
+++ create-ecr-stack.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
     --region us-east-1 \
     --stack-name ExpressApi-ECR \
-    --template-body file://eks-cluster-stack.yaml \
+    --template-body file://ecr-repository-stack.yaml \
     --capabilities CAPABILITY_NAMED_IAM
