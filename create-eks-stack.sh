18aa4a8918c0c3c8c4c28052ccf91b14fbaa74ba
Omar Gabr <ogabr@westmonroe.com>
2023-07-12 18:14:52 -0500

    please god work

--- create-eks-stack.sh
+++ create-eks-stack.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
     --region us-east-1 \
-    --stack-name ExpressApiCluster \
+    --stack-name ExpressApi-EKSCluster \
     --capabilities CAPABILITY_NAMED_IAM \
     --template-body file://eks-cluster-stack.yaml
--- create-vpc-stack.sh
+++ create-vpc-stack.sh
@@ -1,4 +1,21 @@
 aws cloudformation create-stack \
     --region us-east-1 \
-    --stack-name expressapi-vpc \
+    --stack-name ExpressApi-vpc \
     --template-body file://eks-vpc-stack.yaml
+
+aws cloudformation wait stack-create-complete \
+  --region us-east-1 \
+  --stack-name ExpressApi-vpc
+
+stack_status=$(aws cloudformation describe-stacks \
+  --region us-east-1 \
+  --stack-name ExpressApi-vpc \
+  --query 'Stacks[0].StackStatus' \
+  --output text)
+
+if [[ $stack_status == "CREATE_COMPLETE" ]]; then
+  echo "CloudFormation stack creation succeeded!"
+else
+  echo "CloudFormation stack creation failed with status: $stack_status"
+  exit 1
+fi
