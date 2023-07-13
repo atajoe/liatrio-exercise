d936bf2fb9bb201bc9295e749c0ffcb545c30c7f
Omar Gabr <ogabr@westmonroe.com>
2023-07-12 19:35:50 -0500

    please god work

--- create-ecr-stack.sh
+++ create-ecr-stack.sh
@@ -1,5 +1,4 @@
-aws cloudformation create-stack \
+aws ecr create-repository \
+    --repository-name ExpressApi-Repo  \
+    --image-scanning-configuration scanOnPush=true \
     --region us-east-1 \
-    --stack-name ExpressApi-ECR \
-    --template-body file://ecr-repository-stack.yaml \
-    --capabilities CAPABILITY_NAMED_IAM
