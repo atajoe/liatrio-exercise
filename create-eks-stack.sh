066af657cae6af9a016fc86db817fa4cad1facfb
Omar Gabr <ogabr@westmonroe.com>
2023-07-12 22:26:09 -0500

    please god work

--- create-eks-stack.sh
+++ create-eks-stack.sh
@@ -3,3 +3,15 @@ aws cloudformation create-stack \
     --stack-name ExpressApi-EKSCluster \
     --capabilities CAPABILITY_NAMED_IAM \
     --template-body file://eks-cluster-stack.yaml
+
+eks_cluster_status=$(aws eks describe-cluster \
+  --name ExpressApi-EKSCluster \
+  --query 'cluster.status' \
+  --output text)
+
+if [[ $eks_cluster_status == "ACTIVE" ]]; then
+  echo "EKS cluster creation succeeded!"
+else
+  echo "EKS cluster creation failed with status: $eks_cluster_status"
+  exit 1
+fi
