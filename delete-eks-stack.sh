6773c6a5238e598edd059ddb26e2c744f3b7268c
Omar Gabr <ogabr@westmonroe.com>
2023-07-17 23:01:20 -0500

    last chance!

--- delete-eks-stack.sh
+++ delete-eks-stack.sh
@@ -1,3 +1,20 @@
 aws cloudformation delete-stack \
   --region us-east-1 \
   --stack-name ExpressApi-EKSCluster
+
+aws cloudformation wait stack-delete-complete \
+  --stack-name ExpressApi-EKSCluster \
+  --region us-east-1
+
+stack_status=$(aws cloudformation describe-stacks \
+  --region us-east-1 \
+  --stack-name ExpressApi-EKSCluster \
+  --query 'Stacks[0].StackStatus' \
+  --output text)
+  
+if [[ $stack_status == "DELETE_COMPLETE" ]]; then
+  echo "CloudFormation VPC stack deleted!"
+else
+  echo "CloudFormation stack creation failed with status: $stack_status"
+  exit 1
+fi
